7a293843aebb9248763591305bfb69daf1c65a66
Rabbi <golam.rabbi@klovercloud.com>
2022-05-31 23:24:56 +0600

    Twosum with hashmap

--- Two Number Sum/solve1.js
+++ Two Number Sum/solve1.js
@@ -1,14 +1,27 @@
 // Complexity Time = O(n^2) | Space = O(1)
-const array = [3,5,-4,8,11,1,-1,6]; // Target = 10
+const array = [3, 5, -4, 8, 11, 1, -1, 6]; // Target = 10
 
-function twoNumberSum(array, targetSum) {
-    for (let i=0; i < (array.length -1); i++) {
+function withDoubleForLoop(array, targetSum) {
+    for (let i = 0; i < (array.length - 1); i++) {
         firstNum = array[i];
-        for (let j = i+1; j <array.length; j++){
+        for (let j = i + 1; j < array.length; j++) {
             secondNum = array[j];
-            if(firstNum + secondNum === targetSum) {
+            if (firstNum + secondNum === targetSum) {
                 return [array.indexOf(firstNum), array.indexOf(secondNum)];
             }
+        }
+    }
+
+    return [];
+}
+
+function withHashMap(array, targetSum) {
+    let numberHash = {}
+    for (let num of array) {
+        if ((targetSum - num) in numberHash) {
+            return [targetSum - num, num]
+        } else {
+            numberHash[num] = true;
         }
     }
 
